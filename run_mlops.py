5f0b43971fe02914d35e084aa81d29ffa084bde4
Zijian Hu <zjh@fedml.ai>
2023-10-01 00:40:05 -0700

    run_mlops: simplify output piping, cleanup

--- run_mlops.py
+++ run_mlops.py
@@ -1,29 +1,15 @@
-from typing import AnyStr, IO, Optional, Sequence, TextIO
+from typing import Optional, Sequence
 
 from argparse import ArgumentParser, Namespace
 import os
 from pathlib import Path
-from queue import Queue
-from subprocess import CalledProcessError, PIPE, Popen
+import subprocess
+from subprocess import CalledProcessError
 import sys
-from threading import Thread
 
 import torch.cuda
 
 TORCH_DISTRIBUTED_DEFAULT_PORT = 29500
-
-
-def read_stream(input_stream: IO[AnyStr], message_queue: Queue, output_stream: TextIO) -> None:
-    try:
-        for line in input_stream:
-            message_queue.put_nowait((line, output_stream))
-    except ValueError as e:
-        if not input_stream.closed:
-            # raise exception if the stream is not closed
-            raise e
-    finally:
-        # send closing message
-        message_queue.put_nowait((None, output_stream))
 
 
 def parse_args(args: Optional[Sequence[str]] = None, namespace: Optional[Namespace] = None) -> Namespace:
@@ -141,54 +127,21 @@ def main() -> int:
     print(f"cmd = {cmd}")
     print(f"sys.argv = {sys.argv}")
 
-    # see https://stackoverflow.com/a/28319191
-    message_queue = Queue()
+    proc = subprocess.run(
+        [
+            sys.executable,
+            *cmd,
+            *sys.argv[1:],
+        ],
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+        env=os.environ
+    )
 
-    with Popen(
-            [
-                sys.executable,
-                *cmd,
-                *sys.argv[1:],
-            ],
-            stdout=PIPE,
-            stderr=PIPE,
-            bufsize=1,
-            universal_newlines=True,
-            env=os.environ
-    ) as proc:
-        thread_stdout = Thread(target=read_stream, args=(proc.stdout, message_queue, sys.stdout))
-        thread_stderr = Thread(target=read_stream, args=(proc.stderr, message_queue, sys.stderr))
-        thread_stdout.start()
-        thread_stderr.start()
-
-        is_stdout_done = False
-        is_stderr_done = False
-
-        # Adapted from https://stackoverflow.com/a/4896288 and
-        # and https://stackoverflow.com/a/51668895
-        # and https://stackoverflow.com/a/18422264
-        while not is_stdout_done or not is_stderr_done:
-            line, output_file = message_queue.get()
-            message_queue.task_done()
-
-            if line is not None:
-                print(line, end="", file=output_file, flush=True)
-            elif output_file is sys.stdout:
-                is_stdout_done = True
-            elif output_file is sys.stderr:
-                is_stderr_done = True
-
+    print(f"{__file__} done.")
     if proc.returncode != 0:
         raise CalledProcessError(proc.returncode, proc.args)
 
-    print("Closing IO threads...", flush=True)
-    thread_stdout.join()
-    thread_stderr.join()
-    # see https://superfastpython.com/thread-queue-task-done-join/
-    print("Closing message queue...", flush=True)
-    message_queue.join()
-
-    print(f"{__file__} done.")
     return proc.returncode
 
 
